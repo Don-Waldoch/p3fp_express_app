f8a60aaeeecf170ebb14600df75b7d8e0e53a6ae
unknown <waldoch.don@gmail.com>
2022-08-02 17:17:09 -0500

    SignUp and SignIn routes

---
+++ index.js
@@ -0,0 +1,51 @@
+///////////////////////////////////////////////////////////////////////
+// =======
+// DEPENDENCIES
+const express = require("express");
+const cors = require('cors');
+const path = require('path');
+
+// CONFIGURATION
+require('dotenv').config();
+const app = express();
+
+// MIDDLEWARE
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(cors());
+
+// ROUTES
+app.get("/", (req, res) => {
+  var options = {
+    root: path.join(__dirname)
+  };
+  var fileName = 'README.md';
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      next(err);
+    } else {
+      console.log('Sent:', fileName);
+    }
+  });    
+});
+
+// Controllers:
+//const bookclubsController = require("./controllers/bookclubs_controller.js");
+//app.use("/bookclubs", bookclubsController);
+
+//const booksController = require("./controllers/books_controller.js");
+//app.use("/books", booksController);
+
+//const clubsController = require("./controllers/clubs_controller.js");
+//app.use("/clubs", clubsController);
+
+//const rostersController = require("./controllers/rosters_controller.js");
+//app.use("/rosters", rostersController);
+
+const usersController = require("./controllers/users.js");
+app.use("/users", usersController);
+
+// LISTEN
+app.listen(process.env.PORT, () => {
+  console.log("Listening on port: ", process.env.PORT);
+});
---
+++ utils/dbConnection.js
@@ -0,0 +1,45 @@
+///////////////////////////////////////////////////////////////////////
+// =======
+// DEPENDENCIES
+const path = require('path');
+
+// CONFIGURATION
+//require('dotenv').config({path: '../.env'})
+require('dotenv').config()
+
+const initOptions = {
+  capSQL: true,
+  error(error, e) {
+    if (e.cn) {
+        console.log('CN:', e.cn);
+      console.log('EVENT:', error.message || error);
+    }
+  },
+  query(e) {
+    console.log(e.query);
+  },
+};
+const pgp = require('pg-promise')(initOptions);
+
+const cn = {
+  host:     process.env.DB_HOST,
+  port:     process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
+  user:     process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  ssl: {
+     rejectUnauthorized: false,
+  },
+};
+
+const db = pgp(cn);
+
+db.connect()
+  .then(obj => {
+    obj.done();
+  })
+  .catch(error => {
+    console.log('ERROR FROM DB:========>\n', error.message);
+  });
+
+module.exports = { db, pgp };
